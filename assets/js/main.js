a493a24a2a1750913b3e94e0cab81bbb8d68d59d
Claire Lamman <49574567+cmlamman@users.noreply.github.com>
2026-09-25 19:09:46 -0400

    fixing formatting for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,15 +206,16 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let dots = document.querySelectorAll(".dot, .slide-number");
+  if (slides.length == 0) {return}
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if (dots[slideIndex-1]) {dots[slideIndex-1].classList.add("active");}
 }
